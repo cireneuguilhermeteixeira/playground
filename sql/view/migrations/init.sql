dbfe9009b09f7b26c0cafcbbdd039a6c2a5acd7b
Cireneu Araujo <cireneuguilhermeteixeira>
2025-07-06 18:16:42 -0300

    Adding init sql database tables

---
+++ sql/view/migrations/init.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photos (
+  id SERIAL PRIMARY KEY,
+  base_url TEXT NOT NULL,
+  user_id INTEGER REFERENCES users(id),
+  created_at TIMESTAMP DEFAULT NOW(),
+  updated_at TIMESTAMP DEFAULT NOW()
+);
