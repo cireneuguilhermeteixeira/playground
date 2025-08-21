44b75d6449096a86fbfa5dbcb5c2d5ce68082a0a
cireneuguilhermeteixeira <cireneuguilhermeteixeira@gmail.com>
2025-08-21 12:52:35 -0300

    Adding initial code sctructure for dungeon game server in java 23

---
+++ java/dungeon-game/src/main/java/com/dungeon_game/demo/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.dungeon_game.demo.controllers;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+    @GetMapping
+    public String sayHello() {
+        return "Hello World.";
+    }
+}
