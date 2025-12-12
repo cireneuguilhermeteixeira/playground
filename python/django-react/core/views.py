96e0369d94f3040b3942ecfd8bddc273c7e3b910
cireneuguilhermeteixeira <cireneuguilhermeteixeira@gmail.com>
2025-12-12 09:25:34 -0300

    django setup

---
+++ python/django-react/core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def home(request):
+    initial_props = {
+        "username": request.user.username or "Guest",
+        "pageTitle": "Home Page",
+    }
+    return render(request, "core/home.html", {"initial_props": initial_props})
+
+
+def about(request):
+    initial_props = {
+        "pageTitle": "About Page",
+        "description": "This is a simple Django + React PoC.",
+    }
+    return render(request, "core/about.html", {"initial_props": initial_props})
