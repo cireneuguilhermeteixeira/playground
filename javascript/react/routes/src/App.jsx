4842e53bd7b383dcc1e5661f355e4f3b9edc6ea9
cireneuguilhermeteixeira <cireneuguilhermeteixeira@gmail.com>
2026-02-19 09:22:34 -0300

    creating initial react structure

---
+++ javascript/react/routes/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main className="container">
+      <h1>Routes POC</h1>
+      <p>Start here: simple React app scaffolded for routing experiments.</p>
+    </main>
+  );
+}
