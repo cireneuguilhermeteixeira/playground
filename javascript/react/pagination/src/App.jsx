83366fcffb5b14a598b5170a89e1160a0dec1cd4
cireneuguilhermeteixeira <cireneuguilhermeteixeira@gmail.com>
2026-03-06 10:38:57 -0300

    initial react application for pagination poc

---
+++ javascript/react/pagination/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <h1>Vite + React</h1>
+      <p>Your simple React app is ready.</p>
+    </main>
+  )
+}
+
+export default App
